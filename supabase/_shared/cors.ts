6442dc5c34a897d447449e87a466e9dc3d226ad6
Mateus Silva <mateussilva651@gmail.com>
2025-09-11 14:27:04 -0300

    fix: adjusted cors

--- supabase/_shared/cors.ts
+++ supabase/_shared/cors.ts
@@ -1,4 +1,6 @@
+// supabase/_shared/cors.ts
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
