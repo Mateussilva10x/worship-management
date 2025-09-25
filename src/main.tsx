f1e7d0c1026f711bede96d00be4746cba26a3949
Mateus Silva <mateussilva651@gmail.com>
2025-09-25 09:51:01 -0300

    fix: adjusted provider

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import theme from "./theme/theme.ts";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import { NotificationProvider } from "./contexts/NotificationContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DataProvider } from "./contexts/DataContext.tsx";
 
 const queryClient = new QueryClient();
 
@@ -17,12 +18,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <NotificationProvider>
-          <AuthProvider>
-            <CssBaseline />
-            <App />
-          </AuthProvider>
-        </NotificationProvider>
+        <DataProvider>
+          <NotificationProvider>
+            <AuthProvider>
+              <CssBaseline />
+              <App />
+            </AuthProvider>
+          </NotificationProvider>
+        </DataProvider>
       </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
